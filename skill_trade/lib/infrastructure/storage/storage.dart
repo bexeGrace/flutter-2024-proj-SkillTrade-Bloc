d165ab323daaf4f61edcbd5942a4706b7a762fc7
kalebish <phy.rules.kab@gmail.com>
2024-05-31 23:56:22 +0300

    config changes

--- skill_trade/lib/infrastructure/storage/storage.dart
+++ skill_trade/lib/infrastructure/storage/storage.dart
@@ -22,10 +22,10 @@ class SecureStorage {
   }
 
   Future<void> _initialize() async {
-    await write("endpoint", "192.168.56.1");
+    await write("endpoint", "10.0.2.2");
   }
 
   Future<void> init() async {
-    await write("endpoint", "192.168.56.1");
+    await write("endpoint", "10.0.2.2");
   }
 }
