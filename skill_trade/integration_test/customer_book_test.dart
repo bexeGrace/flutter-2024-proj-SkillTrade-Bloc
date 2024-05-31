448a6c4ee0cb6e4baa4a411f8c94bcad6ff744d9
kalebish <phy.rules.kab@gmail.com>
2024-05-31 22:31:36 +0300

    edited customer_book feature integration test

--- skill_trade/integration_test/customer_book_test.dart
+++ skill_trade/integration_test/customer_book_test.dart
@@ -17,7 +17,6 @@ import 'package:skill_trade/presentation/screens/home_page.dart';
 import 'package:skill_trade/presentation/screens/login_page.dart';
 import 'package:skill_trade/presentation/screens/signup_page.dart';
 import 'package:http/http.dart' as http;
-// class MockAuthBloc extends Mock implements AuthBloc {}
 void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   await SecureStorage.instance.init();
@@ -33,40 +32,33 @@ void main() async {
   group('Customer Login', () {
     testWidgets('Login flow', (WidgetTester tester) async {
       await tester.pumpWidget(MyApp(authRepository: authRepository, customerRepository: customerRepository, bookingsRepository: bookingsRepository));
-      await tester.pumpAndSettle(); // Wait for the app to settle
+      await tester.pumpAndSettle(); 
 
-      // Verify that HomeScreen is displayed
       expect(find.byType(HomeScreen), findsOneWidget);
       expect(find.text('SkillTrade Hub'), findsOneWidget);
 
-      // Navigate to the login page
       final loginButtonFinder = find.text('login');
       await tester.ensureVisible(loginButtonFinder);
       await tester.tap(loginButtonFinder);
       await tester.pumpAndSettle();
       expect(find.byType(LoginPage), findsOneWidget);
 
-      // Enter email
       await tester.enterText(find.bySemanticsLabel('email'), 'john.doe@example.com');
-      // Enter password
+  
       await tester.enterText(find.bySemanticsLabel('password'), 'password123');
 
-      // Dismiss the keyboard
       await tester.testTextInput.receiveAction(TextInputAction.done);
       await tester.pumpAndSettle();
 
-      // Select customer role
       final customerRadioFinder = find.text('Customer').hitTestable();
       await tester.tap(customerRadioFinder);
       await tester.pumpAndSettle();
 
-      // Tap the login button
       final loginButtonFinderForm = find.text('login');
       await tester.ensureVisible(loginButtonFinderForm);
       await tester.tap(loginButtonFinderForm);
       await tester.pumpAndSettle();
 
-      // Verify navigation to CustomerPage
       expect(await find.byType(CustomerPage), findsOneWidget);
     });
   });
