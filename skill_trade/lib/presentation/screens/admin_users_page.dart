fd549c9902335c2a6f9f853eeb11a742ac15ccbd
kalebish <phy.rules.kab@gmail.com>
2024-05-31 21:49:25 +0300

    fixed bug in admin_users_

--- skill_trade/lib/presentation/screens/admin_users_page.dart
+++ skill_trade/lib/presentation/screens/admin_users_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skill_trade/domain/models/customer.dart';
 import 'package:skill_trade/presentation/widgets/customer_tile.dart';
@@ -18,7 +17,7 @@ class CustomersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    BlocProvider.of<CustomerBloc>(context).add(LoadAllCustomers());
+    BlocProvider.of<CustomerBloc>(context).add(const LoadAllCustomers());
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
